0a02b805006a549c809f277e814c27940415ca5a
Vlad Shahov <vshahov74@gmail.com>
2022-08-31 20:46:51 +0300

    Try

---
+++ app.js
@@ -0,0 +1,10 @@
+// Змінні
+const firstName = 'Vladyslav'
+const Lastname='Shakhov'
+const age=26
+// name ='Vlad'
+// alert('імя людини:' + firstName +',а вік людини:'+age )
+// Мутування
+const LastName=prompt('введіть прізвище')
+alert(firstName+''+LastName)
+// Оператори
