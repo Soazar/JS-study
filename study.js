6b2be5d6402c8ff6861142dfe3654a163dcc81da
Vlad Shahov <vshahov74@gmail.com>
2022-09-05 23:23:08 +0300

    refactoring

---
+++ study.js
@@ -0,0 +1,40 @@
+
+/* alert('Hello');
+[1, 2].forEach(alert); */
+
+
+// let message = 'Hello!';
+// Змінні
+/* let user = 'John';
+let age = 25;
+let message = 'Hello';
+
+alert(message);
+[user, age].forEach(alert); */
+
+// Сталі
+// const COLOR_RED = "#F00";
+// const COLOR_GREEN = "#0F0";
+// const COLOR_BLUE = "#00F";
+// const COLOR_ORANGE = "#FF7F00";
+
+// let color = COLOR_ORANGE;
+// alert(color);
+/* let name = 'Jhon';
+let admin = 'Jhon';
+alert(admin); */
+
+let admin, name; // можно объявить две переменные через запятую
+
+name = "Джон";
+
+admin = name;
+
+alert(admin); // "Джон"
+
+const ourPlanet = 'Earth'
+const userName='Vladyslav' 
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);
+ 
